0c908cf9a3f5520a687f30523d2319d5ef6ebee1
Yauheni <yauheni.linkevich@gmail.com>
2026-02-11 23:45:28 -0500

    Fixed test coverage for AdminControllerTest

--- src/test/java/com/myapp/Airports/controller/web/AdminControllerTest.java
+++ src/test/java/com/myapp/Airports/controller/web/AdminControllerTest.java
@@ -1,9 +1,12 @@
 package com.myapp.Airports.controller.web;
 
 import com.myapp.Airports.controller.web.AdminController;
+import com.myapp.Airports.service.BookingService;
+import com.myapp.Airports.service.FlyingService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -17,6 +20,12 @@ class AdminControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private FlyingService flyingService;
+
+    @MockBean
+    private BookingService bookingService;
 
     @Test
     void shouldReturnAdminIndexView() throws Exception {
