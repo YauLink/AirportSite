a7ed2630443b74c25bf8921860728ddab4ad4eac
Yauheni <yauheni.linkevich@gmail.com>
2026-02-26 22:18:29 -0500

    Small updates in DTO logic

--- src/main/java/com/myapp/Airports/dto/AuthRequestDTO.java
+++ src/main/java/com/myapp/Airports/dto/AuthRequestDTO.java
@@ -1,8 +1,16 @@
 package com.myapp.Airports.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class AuthRequestDTO {
 
+    @NotBlank(message = "Username is required")
+    @Size(max = 50, message = "Username must be up to 50 characters")
     private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(max = 100, message = "Password must be up to 100 characters")
     private String password;
 
     public AuthRequestDTO() {}
--- src/main/java/com/myapp/Airports/dto/AuthResponseDTO.java
+++ src/main/java/com/myapp/Airports/dto/AuthResponseDTO.java
@@ -1,8 +1,16 @@
 package com.myapp.Airports.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class AuthResponseDTO {
 
+    @NotNull(message = "User ID is required")
     private Long userId;
+
+    @NotBlank(message = "Full name is required")
+    @Size(max = 100, message = "Full name must be up to 100 characters")
     private String fullName;
 
     public AuthResponseDTO() {}
